cdad58dc6dfa7b71ad2e81b65f229d52d2a6ff79
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-08-24 20:25:06 +0800

    Refactor function calls to use alias for utils

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import utils.basic as *
-set_ph(1)
+import utils.basic as t
+t.set_ph(1)
 # Start
-tt("Hey?", 1, 0)
-tt("Wake up", 1, 0)
-tt("The time isn't right yet", 1, 0)
-tt("Emm...", 1, 0)
-tt("Your name is Bbl15-Antagonisms, right?", 1, 0)
-tt("Oh, and can I know more about you?", 1, 0)
-tt("Emm... okay", 1, 0)
+t.tt("Hey?", 1, 0)
+t.tt("Wake up", 1, 0)
+t.tt("The time isn't right yet", 1, 0)
+t.tt("Emm...", 1, 0)
+t.tt("Your name is Bbl15-Antagonisms, right?", 1, 0)
+t.tt("Oh, and can I know more about you?", 1, 0)
+t.tt("Emm... okay", 1, 0)
