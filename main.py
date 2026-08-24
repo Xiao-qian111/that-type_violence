fff7c14a4aac3630014bb1671567448520298b87
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-08-24 16:18:18 +0800

    Implement centered title, header, and subheader functions
    Added functions to center align titles, headers, and subheaders.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title("test")
+
+def ct(text):
+    st.title(text, text_alignment = "center")
+
+def ch(text):
+    st.header(text, text_alignment = "center")
+
+def cs(text):
+    st.subheader(text, text_alignment = "center")
+
+ct("test")
