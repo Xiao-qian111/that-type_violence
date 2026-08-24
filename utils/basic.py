bba0ec4eedfdadba3f67d6b45d85bfbe0c9284b3
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-08-24 20:01:04 +0800

    Implement Streamlit display functions in basic.py
    Added functions for displaying titles, headers, and subheaders in Streamlit with center alignment.

--- utils/basic.py
+++ utils/basic.py
@@ -1,1 +1,22 @@
-# wait
+import streamlit as st
+import time
+def ct(text):
+    st.title(text, text_alignment = "center")
+
+def ch(text):
+    st.header(text, text_alignment = "center")
+
+def cs(text):
+    st.subheader(text, text_alignment = "center")
+
+def tt(text, time):
+    ct(text)
+    time.sleap(time)
+
+def th(text, time):
+    ch(text)
+    time.sleap(time)
+
+def ts(text, time):
+    cs(text)
+    time.sleap(time)
