99abeb397815d3fb9d5a39f655b11a62a74bdb80
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-08-24 19:56:31 +0800

    Refactor main.py by removing unused functions

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 import streamlit as st
-
-def ct(text):
-    st.title(text, text_alignment = "center")
-
-def ch(text):
-    st.header(text, text_alignment = "center")
-
-def cs(text):
-    st.subheader(text, text_alignment = "center")
-
-ct("test")
+import utils.basic
