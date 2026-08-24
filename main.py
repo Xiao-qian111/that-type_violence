45a0daa3ce4c027c2b1506b3b5b041d012a83d54
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-08-24 20:24:21 +0800

    Implement initial user dialogue in main.py
    Added dialogue prompts for user interaction.

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import streamlit as st
-import utils.basic
+import utils.basic as *
+set_ph(1)
+# Start
+tt("Hey?", 1, 0)
+tt("Wake up", 1, 0)
+tt("The time isn't right yet", 1, 0)
+tt("Emm...", 1, 0)
+tt("Your name is Bbl15-Antagonisms, right?", 1, 0)
+tt("Oh, and can I know more about you?", 1, 0)
+tt("Emm... okay", 1, 0)
