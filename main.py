724f6ad1d5bf3e584788bf8be496ee5616432341
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-09-18 21:30:06 +0800

    Split long strings into multiple lines
    Refactor long strings into multiple lines for readability.

--- main.py
+++ main.py
@@ -9,9 +9,21 @@ lines = [
     "Emm...",
     "Can I know more about you?",
     "Emm... okay",
-    "6YKj576k572R5pq06ICF5b6I6K6o5Y6M77yM5LiN5piv5ZCX77yf",
-    "QmJsMTUtQW50YWdvbmlzbXPvvIzkuI3opoHnkIbkvJrku5bku6zvvIzotbDlpb3oh6rlt7HnmoTot6/vvIE=",
-    "572R5pq0NOmprO+8jOe9keaatOaIkeiNieazpemprO+8jOaIkeiNieatu+S9oOWFqOWutu+8jOaIkeWOu+S9oOWmiOeahOmalOWjgeeahOWPuOmprOWCu+mAvO+8jOS9oOS7rOS4jemFjea0u+edgO+8jOaZi+acneS6uuWPuOmprOWutu+8jOi/meS5iOivtOmDveS7luWmiOS+rui+sei/meS6m+ivje+8jOaIkeS7luWmiOS7peWQjuWGjeS5n+WmiOeahOmalOWjgeeahOeUqOS4jeS6hui/meS6m+ivjeS6huWlveS4jeWlve+8jOaTjeS9oOWmiOeahOWCu+mAvA=="
+    "6YKj576k572R5pq06ICF5b6I6K6o5Y6M77y\n
+    M5LiN5piv5ZCX77yf",
+    "QmJsMTUtQW50YWdvbmlzbXPvvIzkuI3opoH\n
+    nkIbkvJrku5bku6zvvIzotbDlpb3oh6rlt7\n
+    HnmoTot6/vvIE=",
+    "572R5pq0NOmprO+8jOe9keaatOaIkeiNiea\n
+    zpemprO+8jOaIkeiNieatu+S9oOWFqOWutu\n
+    +8jOaIkeWOu+S9oOWmiOeahOmalOWjgeeah\n
+    OWPuOmprOWCu+mAvO+8jOS9oOS7rOS4jemF\n
+    jea0u+edgO+8jOaZi+acneS6uuWPuOmprOW\n
+    utu+8jOi/meS5iOivtOmDveS7luWmiOS+ru\n
+    i+sei/meS6m+ivje+8jOaIkeS7luWmiOS7p\n
+    eWQjuWGjeS5n+WmiOeahOmalOWjgeeahOeU\n
+    qOS4jeS6hui/meS6m+ivjeS6huWlveS4jeW\n
+    lve+8jOaTjeS9oOWmiOeahOWCu+mAvA=="
 ]
 
 code = f"""
