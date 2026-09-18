52f2fd611dc8db8504ceddd270f1dc771aa93c8f
SCXG <144230175+Xiao-qian111@users.noreply.github.com>
2026-09-18 21:19:55 +0800

    Add new string entries to the list in main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ lines = [
     "Emm...",
     "Can I know more about you?",
     "Emm... okay",
+    "6YKj576k572R5pq06ICF5b6I6K6o5Y6M77yM5LiN5piv5ZCX77yf",
+    "QmJsMTUtQW50YWdvbmlzbXPvvIzkuI3opoHnkIbkvJrku5bku6zvvIzotbDlpb3oh6rlt7HnmoTot6/vvIE=",
+    "572R5pq0NOmprO+8jOe9keaatOaIkeiNieazpemprO+8jOaIkeiNieatu+S9oOWFqOWutu+8jOaIkeWOu+S9oOWmiOeahOmalOWjgeeahOWPuOmprOWCu+mAvO+8jOS9oOS7rOS4jemFjea0u+edgO+8jOaZi+acneS6uuWPuOmprOWutu+8jOi/meS5iOivtOmDveS7luWmiOS+rui+sei/meS6m+ivje+8jOaIkeS7luWmiOS7peWQjuWGjeS5n+WmiOeahOmalOWjgeeahOeUqOS4jeS6hui/meS6m+ivjeS6huWlveS4jeWlve+8jOaTjeS9oOWmiOeahOWCu+mAvA=="
 ]
 
 code = f"""
